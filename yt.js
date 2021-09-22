411957528e43632e51896207a15e65678ea77ff6
szymonhrehorowicz <szymhre@gmail.com>
2021-09-22 15:52:17 +0200

    update

--- yt.js
+++ yt.js
@@ -1,5 +1,5 @@
 // Script handles accept cookies policies popup on youtube webpage
 let dialog = document.querySelector('#dialog');
 if (dialog) {
-    dialog.children['content'].children[3].children[1].click();
+    dialog.children['content'].children[2].children[4].children[1].children[1].click();
 }
